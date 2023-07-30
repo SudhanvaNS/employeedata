8a3f42388693a52d5e41a1a5b5e31052d0d818ab
Sri_Krishna_G <srikrishnag28@gmail.com>
2023-07-31 01:21:04 +0530

    added login and registertaion

--- database.sql
+++ database.sql
@@ -1,10 +1,16 @@
-create database emp;
-use emp;
-create table emp_data (
-    id int primary key,
+create database BIT_ISE;
+use BIT_ISE;
+create table students (
+    usn varchar(10) primary key,
     name varchar(100) not null,
     phone varchar(15),
-    address varchar(100),
-    working bool not null default false,
-    departments varchar(100)
+    gender enum('Male', 'Female');
+    teacher_id INT,
+    FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
+);
+
+CREATE TABLE teacher (
+    teacher_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    password varchar(20)
 );
